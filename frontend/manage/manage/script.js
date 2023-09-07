505206ff3a14ad2ae28a8634ce2fb8e57570429b
Ardasher <104435462+B1teCode@users.noreply.github.com>
2023-09-07 18:37:45 +0300

    Delete frontend directory

--- frontend/manage/manage/script.js
+++
@@ -1,9 +0,0 @@
-const btn = document.querySelector("#menu-btn");
-const nav = document.querySelector("#menu");
-console.log(btn)
-console.log(nav)
-btn.addEventListener("click", () => {
-	btn.classList.toggle("open");
-	nav.classList.toggle("flex");
-	nav.classList.toggle("hidden");
-});
